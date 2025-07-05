7d01eb525fa151c21083bb76454e237373660c29
milan <pronicmilan@gmail.com>
2025-07-05 02:03:50 +0200

    show details of company by company id

---
+++ Hair.Application/Companies/Queries/CompanyDetailsByIdQuery.cs
@@ -0,0 +1,16 @@
+﻿using Hair.Application.Common.Dto.Company;
+using Hair.Application.Common.Interfaces;
+using MediatR;
+
+namespace Hair.Application.Companies.Queries;
+
+public record CompanyDetailsByIdQuery(Guid CompanyId) : IRequest<CompanyDetailsDto>;
+
+public class CompanyDetailsByIdQueryHandler(ICompanyService companyService) : IRequestHandler<CompanyDetailsByIdQuery, CompanyDetailsDto>
+{
+    public async Task<CompanyDetailsDto> Handle(CompanyDetailsByIdQuery request, CancellationToken cancellationToken)
+    {
+        var companyDetailsById = await companyService.GetCompanyDetailsById(request.CompanyId, cancellationToken);
+        return companyDetailsById;
+    }
+}
