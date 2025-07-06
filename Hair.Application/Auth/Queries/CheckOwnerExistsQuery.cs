6cde3cf249ddb89bae70be10b72ee66b1b57d5d7
milan <pronicmilan@gmail.com>
2025-07-06 19:32:36 +0200

    +

---
+++ Hair.Application/Auth/Queries/CheckOwnerExistsQuery.cs
@@ -0,0 +1,24 @@
+﻿using Hair.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Hair.Application.Auth.Queries;
+
+public record CheckOwnerExistsQuery(Guid CompanyId): IRequest<bool>;
+
+public class CheckOwnerExistsQueryHandler(IAuthService authService) : IRequestHandler<CheckOwnerExistsQuery, bool>
+{
+    public async Task<bool> Handle(CheckOwnerExistsQuery request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var checkIfExists = await authService.CheckIfCompanyOwnerExistsAsync(request.CompanyId, cancellationToken);
+            return checkIfExists;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
